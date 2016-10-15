10451fcfcd5cd8b0650bafeb15926cd8c3a41f1f
39M <xwszc7@gmail.com>
2016-10-15 16:01:21 +0800

    scripts

---
+++ Assets/Scripts/NoteBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteBehaviour : MonoBehaviour {
+	public GameManager gm;
+	float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, -speed * Time.deltaTime, 0);
+	}
+
+	public void Init(GameManager gameManager)
+	{
+		gm = gameManager;
+		speed = gm.noteSpeed;
+	}
+}
